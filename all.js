7d87c4b43bd48997975a3ac41887ad21b2f4fa17
mkomnen <maridar97@yahoo.com>
2019-01-09 18:02:07 +0100

    ...

--- all.js
+++ all.js
@@ -1,25 +1,73 @@
 $(document).ready(function(){
-  $('#topnav').onePageNav({
-    currentClass: 'current',
-      scrollOffset: 0
-  });
+  // $('#topnav').onePageNav({
+  //   currentClass: 'current',
+  //     scrollOffset: 0
+  // });
+
+
+  var footnotediv = $( '#footnotes' );
+  $( '.footnote-ref' ).click( function( event ) {
+    event.preventDefault( );
+
+    var link = $( this );
+    var target = $( '#' + link.data( 'link' ) );
+
+    footnotediv.scrollTop( footnotediv.scrollTop( )
+         + target.position( ).top );
+
+    footnotediv.find( '*' ).not( target ).css( 'color', '#fff' );
+    target.css( 'color', '#f0f' );
+  } );
+
+  $( '#main' ).scroll( function( ) {
+    footnotediv.find( '*' ).css( 'color', '#fff' );
+  } );
+
+
+  function openLarge( ) {
+    $( '#image-large' ).css( { 'background-image':
+                               'url(' + $( this ).attr( 'src' ) + ')',
+                              'display': 'inline-block' } );
+  }
+  $( '#images img' ).click( openLarge );
+
+
+  function closeLarge( ) {
+    $( '#image-large' ).css( 'display', '' );
+  }
+
+  $( '#image-large' ).click( closeLarge );
+
+  $( document ).keyup( function( event ) {
+    if ( event.keyCode == 27 ) closeLarge( );
+  } );
 });
 
-$('#nav').onePageNav({
-    currentClass: 'current',
-    changeHash: false,
-    scrollSpeed: 750,
-    scrollOffset: 30,
-    scrollThreshold: 0.5,
-    filter: '',
-    easing: 'swing',
-    begin: function() {
-        //I get fired when the animation is starting
-    },
-    end: function() {
-        //I get fired when the animation is ending
-    },
-    scrollChange: function($currentListItem) {
-        //I get fired when you enter a section and I pass the list item of the section
-    }
-});
+
+
+function popup( mypage ) {
+    var popup = document.getElementById( mypage );
+    popup.classList.toggle("show");
+}
+function showPrintPreview ( event ) {
+    event.preventDefault( );
+    //
+    // document.getElementById( 'myPopup' ).classList.remove("show");
+    // document.getElementById( 'myPopup2' ).classList.remove("show");
+
+    var printoverlay = document.getElementById( 'printoverlay' );
+    printoverlay.classList.toggle( "show" );
+
+    var printpreview = document.getElementById( 'printpreview' );
+    printpreview.classList.toggle( "show" );
+}
+function hidePrintPreview ( ) {
+    document.getElementById( 'printoverlay' ).classList.remove("show");
+    document.getElementById( 'printpreview' ).classList.remove("show");
+}
+document.onkeyup = function ( event ) {
+    if ( event.key.toLowerCase( ) == 'escape' )
+        hidePrintPreview( );
+}
+
+document.getElementById( 'printoverlay' ).onclick = hidePrintPreview;
